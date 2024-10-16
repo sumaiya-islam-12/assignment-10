465618f0465bbd977ae60d9c0d5ae30a2b446459
Your Name <sumaiyaislam9997@gmail.com>
2024-10-16 11:37:35 +0600

    first commit

---
+++ for-loop.js
@@ -0,0 +1,24 @@
+// var numbers = [1, 2, 22, 222, 3, 33, 333, 4, 44, 444 ];
+
+// for(var i = 1; i <= numbers.length; i++){
+
+//     var element = numbers[i];
+//     console.log(element);
+
+// }
+
+
+
+
+for (var i = 0; i < 100; i++) {
+    console.log("খরগোশ");
+    console.log(i)
+
+}
+
+
+// for(var NaruGiven = 0; NaruGiven < 10; NaruGiven++){
+//     console.log("Rikta, Naru den");
+//     console.log(NaruGiven);
+
+// }
---
+++ script.js
@@ -0,0 +1,20 @@
+var Sumaiya = 20;
+var Shanti = 70;
+var sumu = 35;
+var Sumaan = 37;
+
+
+
+
+if (Sumaiya < 14) {
+    console.log("picsart");
+}else if( Shanti < 65){
+    console.log("canva");
+}else if(sumu > 43){
+    console.log("Ai Editor");
+}else if(Sumaan > 56){
+    console.log("Google Ai Editor");
+
+}else{
+    console.log("Photoshop")
+}
